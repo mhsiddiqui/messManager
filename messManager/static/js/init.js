512497f6c6750127f4748b0080bd18316c44e9c8
mhassan <mhassan.qc@gmail.com>
2015-05-21 01:05:21 +0500

    admin panel layout

--- messManager/static/js/init.js
+++ messManager/static/js/init.js
@@ -2,17 +2,27 @@
   $(function(){
 
     $('.button-collapse').sideNav({
-      menuWidth: 240 // Default is 240
+      menuWidth: 228 // Default is 240
     });
      $('.parallax').parallax();
      $('.modal-trigger').leanModal();
      $(".dropdown-button").dropdown( {});
-     var top_man_height = $('#nav-bar-wrap').height();
-     $('#nav-mobile').css('margin-top',top_man_height);
+      var window_width = $(window).width();
+          if (window_width<434){
+                  $('.brand-logo').css('font-size','15pt');
+                }
+          else{
+              $('.brand-logo').css('font-size','31.5px');
+          }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 $( window ).resize(function() {
-          var top_man_height = $('#nav-bar-wrap').height();
-          $('#nav-mobile').css('margin-top',top_man_height);
+          var window_width = $(window).width();
+          if (window_width<=434){
+                  $('.brand-logo').css('font-size','15pt');
+                }
+          else{
+              $('.brand-logo').css('font-size','31.5px');
+          }
 });
