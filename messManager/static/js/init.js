a1fde09bada3700d5d7420106e3316a3f28b559f
M Hassan Siddiqui <mhassan.qc@gmail.com>
2015-05-21 20:00:38 +0500

    further work, Profile pic thumnail in admin panel

--- messManager/static/js/init.js
+++ messManager/static/js/init.js
@@ -14,15 +14,36 @@
           else{
               $('.brand-logo').css('font-size','31.5px');
           }
+      $("#login_info_div").hide();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 $( window ).resize(function() {
           var window_width = $(window).width();
           if (window_width<=434){
-                  $('.brand-logo').css('font-size','15pt');
+                  $('.brand-logo').css('font-size','14pt');
                 }
           else{
               $('.brand-logo').css('font-size','31.5px');
           }
+
 });
+function show_login_div(element)
+{
+	var offset = $( element ).offset();
+	var height = $(element).height();
+	var top_position = offset.top+height;
+    var adj = 2;
+    if($(window).width()<600){
+        top_position = 48;
+        adj = 0;
+    }
+	if ($('#login_info_div').css('display') == 'none') {
+	 $("#login_info_div" ).css({ top:top_position+3, right:offset.top-adj });
+	 $("#login_info_div").show();
+	 }
+	 else
+	 {
+	   $("#login_info_div").hide();
+	 }
+}
