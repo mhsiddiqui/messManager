de85549a3ae6ebe998037aec97340f9b8ad86730
M Hassan Siddiqui <mhassan.qc@gmail.com>
2015-05-25 12:12:39 +0500

    a

--- messManager/models.py
+++ messManager/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.contrib.auth.models import User, Group
 
 from django.db import models
-from django.utils import timesince
+from django.utils import timesince, timezone
 
 
 
